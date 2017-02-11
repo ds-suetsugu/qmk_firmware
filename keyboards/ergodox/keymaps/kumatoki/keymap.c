3742446820056d26ac29ccb2a7f2471795bc34a2
kumatoki <higumaido@gmail.com>
2017-02-12 08:17:04 +0900

    delete esc from lctrl/esc

--- keyboards/ergodox/keymaps/kumatoki/keymap.c
+++ keyboards/ergodox/keymaps/kumatoki/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+-----+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | TAB    |  Q  |   W  |   E  |   R  |   T  |  `~  |           |  [{  |   Y  |   U  |   I  |   O  |  P   |   \|   |
  * |--------+-----+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |ESC/Ctrl|  A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;:  |   '"   |
+ * | Ctrl   |  A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;:  |   '"   |
  * |--------+-----+------+------+------+------|  \|  |           |  ]}  |------+------+------+------+------+--------|
  * | LShift |  Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |  ,<  |  .>  |  /?  | RShift |
  * `--------+------+------+------+------+------------'           `-------------+------+------+------+------+--------'
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_ESC,         KC_1,           KC_2,    KC_3,    KC_4,     KC_5,   KC_6,
         KC_TAB,         KC_Q,           KC_W,    KC_E,    KC_R,     KC_T,   KC_GRV,
-        CTL_T(KC_ESC),  LT(MDIA,KC_A),  KC_S,    KC_D,    KC_F,     KC_G,
+        KC_LCTL,        LT(MDIA,KC_A),  KC_S,    KC_D,    KC_F,     KC_G,
         KC_LSFT,        CTL_T(KC_Z),    KC_X,    KC_C,    KC_V,     KC_B,   KC_BSLS,
         MO(MDIA),       MO(SYMB),       KC_LALT, KC_LGUI, KC_LANG2,
                                                            CTL_T(KC_ESC), TG(SYMB),
