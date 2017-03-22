bab14750472cbc3eb8f0d3335c3a9584dd79cfb2
kmtoki <higumaido@gmail.com>
2017-03-22 20:44:42 +0900

    fix z/gui to z/ctl

--- keyboards/ergodox/keymaps/kmtoki/keymap.c
+++ keyboards/ergodox/keymaps/kmtoki/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS,          KC_1,             KC_2,      KC_3,      KC_4,     KC_5,                KC_TRNS,
         KC_TAB,           KC_Q,             KC_W,      KC_E,      KC_R,     KC_T,                TG(SYMB),
         CTL_T(KC_ESC),    LT(MDIA,KC_A),    KC_S,      KC_D,      KC_F,     KC_G,
-        KC_LSFT,          MT(KC_LGUI,KC_Z), KC_X,      KC_C,      KC_V,     KC_B,                KC_TAB,
+        KC_LSFT,          MT(KC_LCTL,KC_Z), KC_X,      KC_C,      KC_V,     KC_B,                KC_TAB,
         KC_LANG2,         KC_GRV,           KC_BSLS,   KC_LGUI,   MO(SYMB),
                                                                             MT(KC_LALT, KC_ESC), ALL_T(KC_NO),
                                                                                                  KC_HOME,
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_TRNS,     KC_6,             KC_7,      KC_8,      KC_9,     KC_0,                KC_TRNS,
              TG(MDIA),    KC_Y,             KC_U,      KC_I,      KC_O,     KC_P,                KC_MINS,
                           KC_H,             KC_J,      KC_K,      KC_L,     LT(MDIA,KC_SCLN),    KC_QUOT,
-             KC_BSPC,     KC_N,             KC_M,      KC_COMM,   KC_DOT,   MT(KC_RGUI,KC_SLSH), KC_EQL,
+             KC_BSPC,     KC_N,             KC_M,      KC_COMM,   KC_DOT,   MT(KC_RCTL,KC_SLSH), KC_EQL,
                                             MO(SYMB),  KC_RGUI,   KC_LBRC,  KC_RBRC,             KC_LANG1,
              MEH_T(KC_NO), MT(KC_RALT, KC_ESC),
              KC_PGUP,
@@ -72,27 +72,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
-// SYMBOLS
-//[SYMB] = KEYMAP(
-//       // left hand
-//       KC_TRNS,   KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
-//       KC_TRNS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_AMPR,KC_TRNS,
-//       KC_TRNS,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
-//       KC_TRNS,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
-//       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-//                                       RGB_MOD,KC_TRNS,
-//                                               KC_TRNS,
-//                               RGB_VAD,RGB_VAI,KC_TRNS,
-//       // right hand
-//       KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
-//       KC_TRNS, KC_MINS, KC_7,   KC_8,    KC_9,    KC_SLSH, KC_F12,
-//                KC_PIPE, KC_4,   KC_5,    KC_6,    KC_ASTR, KC_SLSH,
-//       KC_TRNS, KC_BSLS, KC_1,   KC_2,    KC_3,    KC_MINS, KC_EQL,
-//                         KC_TRNS,KC_DOT,  KC_0,    KC_PLUS,  KC_TRNS,
-//       RGB_TOG, KC_TRNS,
-//       KC_TRNS,
-//       KC_TRNS, RGB_HUD, RGB_HUI
-//  ),
 
   [SYMB] = KEYMAP(
        // left hand
