6f1cdd81a6d901810c99d7cfde17aadf28abfa74
kmtoki <higumaido@gmail.com>
2017-03-23 22:22:26 +0900

    layer1 swap [] {}

--- keyboards/ergodox/keymaps/kmtoki/keymap.c
+++ keyboards/ergodox/keymaps/kmtoki/keymap.c
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |         |   !  |   @  |   #  |   $  |   %  |------|           |------|   ^  |   &  |   *  |   (  |   )  |        |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |         |   `  |   ~  |   _  |   -  |   |  |      |           |      |   \  |   {  |   }  |   [  |   ]  |        |
+ * |         |   `  |   ~  |   \  |   |  |   -  |      |           |      |   _  |   [  |   ]  |   {  |   }  |        |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |       |      |      |      |      |                                       |      |      |      |      |      |
  *   `-----------------------------------'                                       `----------------------------------'
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,   KC_F1,     KC_F2,     KC_F3,     KC_F4,      KC_F5,     KC_F6,
        KC_TRNS,   KC_1,      KC_2,      KC_3,      KC_4,       KC_5,      KC_TRNS,
        KC_TRNS,   KC_EXLM,   KC_AT,     KC_SLEP,   KC_DLR,     KC_PERC,
-       KC_TRNS,   KC_GRV,    KC_TILD,   KC_UNDS,   KC_MINS,    KC_PIPE,   KC_TRNS,
+       KC_TRNS,   KC_GRV,    KC_TILD,   KC_BSLS,   KC_PIPE,    KC_MINS,  KC_TRNS,
        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,    
                                                                RGB_MOD,   KC_TRNS,
                                                                           KC_TRNS,
@@ -87,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,     KC_F12,    KC_TRNS,
        KC_TRNS,   KC_6,      KC_7,      KC_8,      KC_9,       KC_0,      KC_TRNS,
                   KC_CIRC,   KC_AMPR,   KC_ASTR,   KC_LPRN,    KC_RPRN,   KC_TRNS,
-       KC_TRNS,   KC_BSLS,   KC_LCBR,   KC_RCBR,   KC_LBRC,    KC_RBRC,   KC_TRNS,
+       KC_TRNS,   KC_UNDS,   KC_LBRC,   KC_RBRC,   KC_LCBR,    KC_RCBR,   KC_TRNS,
                   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,
        RGB_TOG,   KC_TRNS,
        KC_TRNS,
