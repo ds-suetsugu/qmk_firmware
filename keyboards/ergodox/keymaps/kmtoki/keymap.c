3c49a918d5e62400a0e32716329c77eb7321a0eb
kmtoki <higumaido@gmail.com>
2017-05-06 22:18:40 +0900

    hogehoge

--- keyboards/ergodox/keymaps/kmtoki/keymap.c
+++ keyboards/ergodox/keymaps/kmtoki/keymap.c
@@ -8,6 +8,8 @@
 #define MDIA 2 // media keys
 #define NEXTTAB ACTION_MODS_KEY(MOD_LGUI, KC_RCBR)
 #define PREVTAB ACTION_MODS_KEY(MOD_LGUI, KC_LCBR)
+#define NEXTWIN ACTION_MODS_KEY(MOD_LGUI, KC_TAB)
+#define PREVWIN ACTION_MODS_KEY(MOD_LSFT | MOD_LGUI, KC_TAB)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer:
@@ -15,20 +17,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * .-------------------------------------------------.           .--------------------------------------------------.
  * |        |  1! |   2@ |   3# |   4$ |   5% |      |           |      |   6^ |   7& |   8* |  9(  |  0)  |        |
  * |--------+-----+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | `~     |  Q  |   W  |   E  |   R  |   T  |Cmd-{ |           |Cmd-} |   Y  |   U  |   I  |   O  |  P   |   -_   |
+ * | `~     |  Q  |   W  |   E  |   R  |   T  | NTAB |           | PTAB |   Y  |   U  |   I  |   O  |  P   |   -_   |
  * |--------+-----+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | ESC    |A L2 |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |;: L2 |   '"   |
- * |--------+-----+------+------+------+------| Eisu |           | Kana |------+------+------+------+------+--------|
+ * |--------+-----+------+------+------+------|  [{  |           |  ]}  |------+------+------+------+------+--------|
  * | \|     |Z Crl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |  ,<  |  .>  |/? Crl|   =+   |
  * `--------+-----+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |     |      |  Alt |  Cmd |                                       | Cmd  |  Alt |  [{  |  ]}  |      |
+ *   |      |     |  Alt |  Cmd |  L1  |                                       |  L1  |  Cmd |  Alt |      |      |
  *   `---------------------------------'                                       `----------------------------------'
  *                                    ,----------------,       ,---------------,
  *                                    | Esc/Alt | TG1  |       | TG2  |Esc/Alt |
  *                             ,------|---------|------|       |------+--------+------.
- *                             |Space | BSPC    | Home |       | PgUp | Tab    |Enter |
- *                             |/Shift|  /L1    |------|       |------|  /L1   |/Shift|
- *                             |      |         | End  |       | PgDn |        |      |
+ *                             |Space |         |      |       |      |        |Enter |
+ *                             |/Shift|BackSpace|------|       |------|  Tab   |/Shift|
+ *                             |      |         |      |       |      |        |      |
  *                             `-----------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -38,20 +40,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS,         KC_1,             KC_2,      KC_3,      KC_4,     KC_5,                KC_TRNS,
         KC_GRV,          KC_Q,             KC_W,      KC_E,      KC_R,     KC_T,                PREVTAB,
         KC_ESC,          LT(MDIA,KC_A),    KC_S,      KC_D,      KC_F,     KC_G,
-        KC_BSLS,         CTL_T(KC_Z),      KC_X,      KC_C,      KC_V,     KC_B,                KC_LANG2,
-        KC_TRNS,         KC_TRNS,          KC_TRNS,   KC_LALT,   KC_LGUI,
+        KC_BSLS,         CTL_T(KC_Z),      KC_X,      KC_C,      KC_V,     KC_B,                KC_LBRC,
+        KC_TRNS,         KC_TRNS,          KC_LALT,   KC_LGUI,   MO(SYMB),
                                                                                   ALT_T(KC_ESC), TG(SYMB),
-                                                                                                 KC_HOME,
-                                                               SFT_T(KC_SPC), LT(SYMB, KC_BSPC), KC_END,
+                                                                                                 KC_TRNS,
+                                                                         SFT_T(KC_SPC), KC_BSPC, KC_TRNS,
         // right hand
              KC_TRNS,     KC_6,             KC_7,      KC_8,      KC_9,     KC_0,                KC_TRNS,
              NEXTTAB,     KC_Y,             KC_U,      KC_I,      KC_O,     KC_P,                KC_MINS,
                           KC_H,             KC_J,      KC_K,      KC_L,     LT(MDIA,KC_SCLN),    KC_QUOT,
-             KC_LANG1,    KC_N,             KC_M,      KC_COMM,   KC_DOT,   CTL_T(KC_SLSH),      KC_EQL,
-                                            KC_RGUI,   KC_RALT,   KC_LBRC,  KC_RBRC,             KC_TRNS,
+             KC_RBRC,     KC_N,             KC_M,      KC_COMM,   KC_DOT,   CTL_T(KC_SLSH),      KC_EQL,
+                                            MO(SYMB),  KC_RGUI,   KC_RALT,  KC_TRNS,             KC_TRNS,
              TG(MDIA), ALT_T(KC_ESC),
-             KC_PGUP,
-             KC_PGDN, LT(SYMB, KC_TAB), SFT_T(KC_ENT)
+             KC_TRNS,
+             PREVWIN, KC_TAB, SFT_T(KC_ENT)
     ),
 
 /* Keymap 1: Symbol Layer
