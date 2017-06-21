a79c21fffec4f8267e4ddec00d3c8518a443f8db
kmtoki <higumaido@gmail.com>
2017-06-21 15:22:37 +0900

    symbol layer++

--- keyboards/ergodox/keymaps/kmtoki/keymap.c
+++ keyboards/ergodox/keymaps/kmtoki/keymap.c
@@ -15,15 +15,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer:
  *
  * .-------------------------------------------------.           .--------------------------------------------------.
- * |        |  1! |   2@ |   3# |   4$ |   5% |  TG1 |           |  TG2 |   6^ |   7& |   8* |  9(  |  0)  |        |
+ * |        |  1! |   2@ |   3# |   4$ |   5% |      |           |      |   6^ |   7& |   8* |  9(  |  0)  |        |
  * |--------+-----+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | `~     |  Q  |   W  |   E  |   R  |   T  | NTAB |           | PTAB |   Y  |   U  |   I  |   O  |  P   |   -_   |
  * |--------+-----+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | ESC    |A L2 |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |;: L2 |   '"   |
  * |--------+-----+------+------+------+------| Eisu |           | Kana |------+------+------+------+------+--------|
- * | \|     |Z Crl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |  ,<  |  .>  |/? Crl|   =+   |
+ * | \|     |  Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |  ,<  |  .>  |  /?  |   =+   |
  * `--------+-----+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |     |  Alt |  Cmd |  L1  |                                       |  L1  |  Cmd |  Alt |      |      |
+ *   |      |     |  Alt |  Cmd |  L1  |                                       |  L1  |  Cmd |  [{  |  ]}  |      |
  *   `---------------------------------'                                       `----------------------------------'
  *                                    ,----------------,       ,---------------,
  *                                    |         |      |       |      |        |
@@ -37,20 +37,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
   [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_TRNS,         KC_1,             KC_2,      KC_3,      KC_4,     KC_5,   TG(SYMB),
+        KC_TRNS,         KC_1,             KC_2,      KC_3,      KC_4,     KC_5,    KC_TRNS,
         KC_GRV,          KC_Q,             KC_W,      KC_E,      KC_R,     KC_T,    PREVTAB,
         CTL_T(KC_ESC),   LT(MDIA,KC_A),    KC_S,      KC_D,      KC_F,     KC_G,
-        KC_BSLS,         CTL_T(KC_Z),      KC_X,      KC_C,      KC_V,     KC_B,    KC_LANG2,
+        KC_BSLS,         KC_Z,             KC_X,      KC_C,      KC_V,     KC_B,    KC_LANG2,
         KC_TRNS,         KC_TRNS,          KC_LALT,   KC_LGUI,   MO(SYMB),
                                                                            KC_TRNS, KC_TRNS,
                                                                                     KC_TRNS,
                                                             SFT_T(KC_SPC), KC_BSPC, KC_TRNS,
         // right hand
-        TG(MDIA),    KC_6,    KC_7,      KC_8,      KC_9,      KC_0,                KC_TRNS,
+        KC_TRNS,     KC_6,    KC_7,      KC_8,      KC_9,      KC_0,                KC_TRNS,
         NEXTTAB,     KC_Y,    KC_U,      KC_I,      KC_O,      KC_P,                KC_MINS,
                      KC_H,    KC_J,      KC_K,      KC_L,      LT(MDIA,KC_SCLN),    KC_QUOT,
-        KC_LANG1,     KC_N,    KC_M,      KC_COMM,   KC_DOT,    CTL_T(KC_SLSH),      KC_EQL,
-                              MO(SYMB),  KC_RGUI,   KC_RALT,   KC_TRNS,             KC_TRNS,
+        KC_LANG1,    KC_N,    KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,             KC_EQL,
+                              MO(SYMB),  KC_RGUI,   KC_LBRC,   KC_RBRC,             KC_TRNS,
         KC_TRNS, KC_TRNS,
         KC_TRNS,
         KC_TRNS, KC_TAB, SFT_T(KC_ENT)
@@ -63,9 +63,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
  * |         |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |        |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |         |   !  |   @  |   #  |   $  |   %  |------|           |------|   ^  |   &  |   *  |   (  |   )  |        |
+ * |    ~    |   !  |   @  |   #  |   $  |   %  |------|           |------|   ^  |   &  |   *  |   (  |   )  |   `    |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |         |      |      |      |      |      |      |           |      |      |   [  |   ]  |   {  |   }  |        |
+ * |         |   |  |   \  |   _  |   -  |   +  |      |           |      |   =  |   [  |   ]  |   {  |   }  |        |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |       |      |      |      |      |                                       |      |      |      |      |      |
  *   `-----------------------------------'                                       `----------------------------------'
@@ -82,21 +82,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        // left hand
        KC_TRNS,   KC_F1,     KC_F2,     KC_F3,     KC_F4,      KC_F5,     KC_F6,
        KC_TRNS,   KC_1,      KC_2,      KC_3,      KC_4,       KC_5,      KC_TRNS,
-       KC_TRNS,   KC_EXLM,   KC_AT,     KC_HASH,   KC_DLR,     KC_PERC,
-       KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_LCBR,   KC_RCBR,    KC_TRNS,   KC_TRNS,
+       KC_TILD,   KC_EXLM,   KC_AT,     KC_HASH,   KC_DLR,     KC_PERC,
+       KC_TRNS,   KC_PIPE,   KC_BSLS,   KC_UNDS,   KC_MINS,    KC_PLUS,   KC_TRNS,
        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,    
-                                                               RGB_MOD,   KC_TRNS,
+                                                               KC_TRNS,   KC_TRNS,
                                                                           KC_TRNS,
-                                                   RGB_VAD,    RGB_VAI,   KC_TRNS,
+                                                   KC_TRNS,    KC_TRNS,   KC_TRNS,
        // right hand
        KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,     KC_F12,    KC_TRNS,
-       KC_TRNS,   KC_6,      KC_7,      KC_8,      KC_9,       KC_0,      KC_TRNS,
+       KC_TRNS,   KC_6,      KC_7,      KC_8,      KC_9,       KC_0,      KC_GRV,
                   KC_CIRC,   KC_AMPR,   KC_ASTR,   KC_LPRN,    KC_RPRN,   KC_TRNS,
-       KC_TRNS,   KC_TRNS,   KC_LBRC,   KC_RBRC,   KC_LCBR,    KC_RCBR,   KC_TRNS,
+       KC_TRNS,   KC_EQL,    KC_LBRC,   KC_RBRC,   KC_LCBR,    KC_RCBR,   KC_TRNS,
                   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,
-       RGB_TOG,   KC_TRNS,
+       KC_TRNS,   KC_TRNS,
        KC_TRNS,
-       KC_TRNS,   RGB_HUD,   RGB_HUI
+       KC_TRNS,   KC_TRNS,   KC_TRNS
   ),
 
 
