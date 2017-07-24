d347091c9027586c090176554140b500182edc36
kmtoki <higumaido@gmail.com>
2017-07-24 19:29:17 +0900

    change tab to bs in lower

--- keyboards/lets_split/keymaps/kmtoki/keymap.c
+++ keyboards/lets_split/keymaps/kmtoki/keymap.c
@@ -42,14 +42,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------| |------+------+------+------+------+------|
    * |      |   ~  |   `  |   |  |   \  |      | |      |   [  |   ]  |   {  |   }  |      |
    * |------+------+------+------+------+------| |------+------+------+------+------+------|
-   * |      |      |      |      | Eisu |  BS  | | Tab  | Kana |      |      |      |      |
+   * |      |      |      |      | Eisu |  BS  | |  BS  | Kana |      |      |      |      |
    * `-----------------------------------------' `-----------------------------------------'
    */
   [LOWER] = KEYMAP( \
     _______, KC_1,    KC_2,    KC_3,    KC_4,     KC_5,    KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    _______, \
     PREVTAB, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,   KC_PERC, KC_CIRC, KC_AMPR,  KC_ASTR, KC_LPRN, KC_RPRN, NEXTTAB, \
     _______, KC_TILD, KC_GRV, KC_PIPE, KC_BSLS,   _______, _______, KC_LBRC,  KC_RBRC, KC_LCBR, KC_RCBR, _______, \
-    _______, _______, _______, _______, KC_LANG2, KC_BSPC, KC_TAB,  KC_LANG1, _______, _______, _______, _______  \
+    _______, _______, _______, _______, KC_LANG2, KC_BSPC, KC_BSPC, KC_LANG1, _______, _______, _______, _______  \
   ),
 
   /* Raise 
