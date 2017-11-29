65bbf951a989cae3fab1dd44f7c4880d354bc09a
kmtoki <higumaido@gmail.com>
2017-11-29 19:38:14 +0900

    ime toggle

--- keyboards/lets_split/keymaps/kmtoki/keymap.c
+++ keyboards/lets_split/keymaps/kmtoki/keymap.c
@@ -4,19 +4,21 @@
 
 extern keymap_config_t keymap_config;
 
-enum LAYER {
-  QWERTY,
-  LOWER,
-  RAISE,
-  MISC,
-};
+#define QWERTY 0
+#define LOWER 1
+#define RAISE 2
+#define MISC 3
 
 // Fillers to make layering more clear
 #define ____ KC_TRNS
 #define XXXX KC_NO
 
-#define TNEXT LGUI(KC_RCBR)
-#define TPREV LGUI(KC_LCBR)
+#define TPREV ACTION_MODS_KEY(MOD_LGUI, KC_RCBR)
+#define TNEXT ACTION_MODS_KEY(MOD_LGUI, KC_LCBR)
+
+enum my_keycodes {
+  IME_TOG = SAFE_RANGE
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -24,35 +26,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |  =+  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  -_  |
+ * | ESC  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  -_  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Misc | Alt  | GUI  |Lower |Space |Enter |Raise | Left | Down |  Up  |Right |
+ * | IME  | Misc | Alt  | GUI  |Lower |Space |Enter |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [QWERTY] = KEYMAP( \
-  KC_TAB,        KC_Q,     KC_W,    KC_E,    KC_R,      KC_T,          KC_Y,          KC_U,      KC_I,    KC_O,    KC_P,    KC_BSPC, \
-  CTL_T(KC_ESC), LT(MISC,  KC_A),   KC_S,    KC_D,      KC_F,          KC_G,          KC_H,      KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_EQL,        KC_Z,     KC_X,    KC_C,    KC_V,      KC_B,          KC_N,          KC_M,      KC_COMM, KC_DOT,  KC_SLSH, KC_MINS, \
-  KC_LCTL,       MO(MISC), KC_LALT, KC_LGUI, LT(LOWER, KC_LANG2), SFT_T(KC_SPC), SFT_T(KC_ENT), LT(RAISE, KC_LANG1), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  KC_TAB,            KC_Q,     KC_W,    KC_E,    KC_R,      KC_T,          KC_Y,          KC_U,      KC_I,    KC_O,    KC_P,    KC_BSPC, \
+  KC_LCTL, LT(MISC,  KC_A),    KC_S,    KC_D,      KC_F,          KC_G,          KC_H,      KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_ESC,            KC_Z,     KC_X,    KC_C,    KC_V,      KC_B,          KC_N,          KC_M,      KC_COMM, KC_DOT,  KC_SLSH, KC_MINS, \
+  IME_TOG,           MO(MISC), KC_LALT, KC_LGUI, MO(LOWER), SFT_T(KC_SPC), SFT_T(KC_ENT), MO(RAISE), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |      |      |      |      |      |      |   ~  |   |  |   {  |   }  |  |   |
+ * | Del  |      |      |      |      |      |      |   _  |   +  |   {  |   }  |  |   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |ISO ~ |ISO | |      |      |      |
+ * |      |      |      |      |      |      |      |    ~ |    | |      |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [LOWER] = KEYMAP( \
   KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC, \
-  KC_DEL,  ____,    ____,  ____,    ____,   ____,    ____,    KC_TILD,    KC_PIPE,    KC_LCBR, KC_RCBR, KC_PIPE, \
-  ____,    ____,    ____,  ____,    ____,   ____,    ____,    S(KC_NUHS), S(KC_NUBS), ____,    ____,    ____,    \
+  KC_DEL,  ____,    ____,  ____,    ____,   ____,    ____,    KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE, \
+  ____,    ____,    ____,  ____,    ____,   ____,    ____,    KC_TILD,    KC_PIPE, ____,    ____,    ____,    \
   ____,    ____,    ____,  ____,    ____,   ____,    ____,    ____,       ____,       ____,    ____,    ____ \
 ),
 
@@ -60,17 +62,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |      |      |      |      |      |      |   `  |   \  |   [  |   ]  |  \   |
+ * | Del  |      |      |      |      |      |      |   -  |   =  |   [  |   ]  |  \   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |ISO # |ISO / |      |      |      |
+ * |      |      |      |      |      |      |      |   `  |   \  |      |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [RAISE] = KEYMAP( \
   KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  KC_DEL, ____, ____, ____, ____, ____, ____, KC_GRV,  KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
-  ____,   ____, ____, ____, ____, ____, ____, KC_NUHS, KC_NUBS, ____,    ____,    ____,    \
+  KC_DEL, ____, ____, ____, ____, ____, ____, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
+  ____,   ____, ____, ____, ____, ____, ____, KC_GRV,  KC_BSLS, ____,    ____,    ____,    \
   ____,   ____, ____, ____, ____, ____, ____, ____,    ____,    ____,    ____,    ____ \
 ),
 
@@ -94,3 +96,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 )
 
 };
+
+static bool ime_state = false;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case IME_TOG:
+      if (record->event.pressed) {
+        if (ime_state) {
+          register_code(KC_LANG1);
+          unregister_code(KC_LANG1);
+          ime_state = !ime_state;
+        }
+        else {
+          register_code(KC_LANG2);
+          unregister_code(KC_LANG2);
+          ime_state = !ime_state;
+        }
+      }
+      return false;
+      break;
+  }
+  return true;
+}
